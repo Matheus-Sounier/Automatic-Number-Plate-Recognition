aa41d1c388527824d675c872d80ed47133e314c7
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-26 23:58:31 -0400

    chore: adds format license, fixing common OCR mix-ups on char position Adds int to char and char to int, following the mersocur patterns getting the char and int collected from plates

--- src/utils/plate_validator.py
+++ src/utils/plate_validator.py
@@ -5,3 +5,26 @@ _ANTIGA   = re.compile(r'^[A-Z]{3}[0-9]{4}$')             # ABC1234
 
 _CHAR_TO_INT = {'O': '0', 'I': '1', 'J': '3', 'A': '4', 'G': '6', 'S': '5'}
 _INT_TO_CHAR = {'0': 'O', '1': 'I', '3': 'J', '4': 'A', '6': 'G', '5': 'S'}
+
+def format_license(text):
+    """
+    Fixes common OCR mix-ups based on character position
+    """
+    if len(text) != 7:
+        return text
+
+# L L L N L N N
+# L L L N N N N
+    mapping = {
+        0: _INT_TO_CHAR,
+        1: _INT_TO_CHAR,
+        2: _INT_TO_CHAR,
+        3: _CHAR_TO_INT,
+        4: _INT_TO_CHAR,
+        5: _CHAR_TO_INT,
+        6: _CHAR_TO_INT,
+    }
+    result = ''
+    for i, char in enumerate(text):
+        result += mapping[i].get(char, char) or char
+    return result
