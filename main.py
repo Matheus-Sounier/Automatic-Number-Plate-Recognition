9f3765a2549b71dab99aa9d6a9b5278ea1d446ea
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-27 15:33:19 -0400

    chore: adds while loop, taking the current time, print frame count, calling the tracked and license plates to process frames function - starttime calling the persist interval function with its values - call persist disappeared function with its values - shows the 'video' webcam

--- main.py
+++ main.py
@@ -20,5 +20,22 @@ startTime = datetime.now()
 saved_plates = set()
 license_plates = {}
 
+while True:
+    ret, frame = cap.read()
+    if ret:
+        currentTime = datetime.now()
+        count += 1
+        print(f"Frame Number: {count}")
+
+        tracked, license_plates = process_frame(frame, count, model, license_plates)
+        startTime = persist_interval(license_plates, saved_plates, startTime, currentTime)
+        persist_disappeared(license_plates, saved_plates, tracked, currentTime)
+
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(1) & 0xFF == ord('1'):
+            break
+    else:
+        break
+
 cap.release()
 cv2.destroyAllWindows()
